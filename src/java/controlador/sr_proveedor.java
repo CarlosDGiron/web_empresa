808864d6263a5216f2e8a780bb0dcb1333e80f50
CarlosDGiron <carlosdavidgironleonardo@gmail.com>
2023-10-25 07:58:28 -0600

    Hecho:	CRUDs Puesto, Proveedores, Marcas, Cliente y Empleados. 		Login y permisos. Pendientes: 		CRUD Productos, Ventas, Compras, Maestros detalle. 		Reportes. 		Menu dinpamico. 		Menus por arbol.

---
+++ src/java/controlador/sr_proveedor.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Proveedor;
+import modelo.Usuario;
+
+/**
+ *
+ * @author cana0
+ */
+public class sr_proveedor extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title></title>");            
+            out.println("</head>");
+            out.println("<body>");
+            int idUsuario=0;
+            if(request.getSession().getAttribute("idUsuario")!=null){
+                idUsuario=(int)request.getSession().getAttribute("idUsuario");
+            }
+            if(idUsuario!=0){
+                Usuario u=new Usuario();
+                if(u.tienePermisoId(idUsuario, 1)){
+                    //Permisos adecuados
+                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");  
+                    Date date = new Date();  
+                    System.out.println(formatter.format(date));  
+                    Proveedor e = new Proveedor(Integer.parseInt(request.getParameter("txt_id")),request.getParameter("txt_proveedor"),request.getParameter("txt_nit"),request.getParameter("txt_direccion"),request.getParameter("txt_telefono"));
+                    if("Agregar".equals(request.getParameter("btn_agregar"))){
+                        if (e.agregar()==1){
+                        out.println("<h1>Ingreso exitoso.</h1>");
+                        }else{
+                        out.println("<h1>No se pudo ingresar el registro.</h1>");
+                        }
+                    }else if("Modificar".equals(request.getParameter("btn_modificar"))){
+                        if (e.modificar()==1){
+                        out.println("<h1>Registro modificado.</h1>");
+                        }else{
+                        out.println("<h1>No se pudo modificar el registro.</h1>");
+                        }                
+                    }else if("Eliminar".equals(request.getParameter("btn_eliminar"))){
+                        if (e.eliminar()==1){
+                        out.println("<h1>Registro eliminado.</h1>");
+                        }else{
+                        out.println("<h1>No se pudo eliminar el registro.</h1>");
+                        }               
+                    }            
+                    out.println("<a href ='Clientes.jsp'>Regresar</a>");
+                    out.println("</body>");
+                    out.println("</html>");
+                    }
+            }else{
+                    out.println("<h1>Usted no tiene permisos.</h1>");
+                    out.println("</body>");
+                    out.println("</html>");
+            }
+        }
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/modelo/Proveedor.java
@@ -0,0 +1,157 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author cana0
+ */
+public class Proveedor {
+    private int idProveedor;
+    private String proveedor,nit,direccion,telefono;
+    private Conexion c;
+
+    public Proveedor(int idProveedor, String proveedor, String nit, String dirección, String telefono) {
+        this.idProveedor = idProveedor;
+        this.proveedor = proveedor;
+        this.nit = nit;
+        this.direccion = dirección;
+        this.telefono = telefono;
+    }
+
+    public Proveedor() {
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    public int agregar(){
+        try{
+            PreparedStatement parametro;
+            c=new Conexion();
+            c.abrir_conexion();
+            String query="INSERT INTO db_empresa.proveedores(proveedor,nit,dirección,telefono) VALUES(?,?,?,?);";
+            parametro=(PreparedStatement) c.conexionDB.prepareStatement(query);
+            parametro.setString(1,getProveedor());
+            parametro.setString(2,getNit());
+            parametro.setString(3,getDireccion());
+            parametro.setString(4,getTelefono());
+            int ejecutar=parametro.executeUpdate();            
+            c.cerrar_conexion();
+            return ejecutar;
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+            c.cerrar_conexion();
+            return 0;
+        }
+    }
+    
+    public int modificar(){
+        try {
+            c= new Conexion();
+            c.abrir_conexion();
+            PreparedStatement parametro;
+            String query="UPDATE db_empresa.proveedores SET proveedor=?, nit=?, direccion=?, telefono=?, where idProveedor=?;";
+            parametro=(PreparedStatement) c.conexionDB.prepareStatement(query);
+            parametro.setString(1,getProveedor());
+            parametro.setString(2,getNit());
+            parametro.setString(3,getDireccion());
+            parametro.setString(4,getTelefono());
+            parametro.setInt(5,getIdProveedor());
+            int ejecutar=parametro.executeUpdate();
+            c.cerrar_conexion();
+            return ejecutar;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            c.cerrar_conexion();
+            return 0;
+        }
+    }
+    
+    public int eliminar(){        
+        try {
+            c=new Conexion();
+            c.abrir_conexion();
+            PreparedStatement parametro;
+            String query="DELETE FROM db_empresa.proveedores WHERE idProveedor=?;";
+            parametro=(PreparedStatement) c.conexionDB.prepareStatement(query);
+            parametro.setInt(1,getIdProveedor());
+            int ejecutar = parametro.executeUpdate();
+            c.cerrar_conexion();
+            return ejecutar;
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            c.cerrar_conexion();
+            return 0;
+        }
+    }
+    
+    public DefaultTableModel mostrar(){
+        DefaultTableModel model=new DefaultTableModel();
+        c=new Conexion();
+        c.abrir_conexion();
+        try{
+            Puesto p = new Puesto();
+            ResultSet res;
+            String encabezado []={"ID Proveedor","Proveedor","NIT","Dirección","Telefono"};
+            model.setColumnIdentifiers(encabezado);
+            res=c.conexionDB.createStatement().executeQuery("Select * from db_empresa.proveedores;");
+            String datos[]=new String[8];
+            while(res.next()){
+                datos[0]=res.getString("idProveedor");
+                datos[1]=res.getString("proveedor");
+                datos[2]=res.getString("nit");
+                datos[3]=res.getString("direccion");
+                datos[4]=res.getString("telefono");   
+                model.addRow(datos);             
+            }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        c.cerrar_conexion();
+        return model;
+    }
+}
